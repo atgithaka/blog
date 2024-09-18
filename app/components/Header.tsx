89a1f914a0cfc7d59a5f508c1299f95e843b3dc1
atgithaka <atgithaka@gmail.com>
2024-09-18 12:31:58 +0300

    feat: dump

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,8 +1,15 @@
+'use client';
+import { useState } from 'react';
 import Brand from './ui/Brand';
 import Button from './ui/Button';
 import styles from '../page.module.css';
+import Image from 'next/image';
 
 export default function Header({ children }: ChildrenProps) {
+    const [state, setState] = useState(Boolean);
+    function toogleDropdown() {
+        setState(!state);
+    }
     return (
         <header className={styles.header}>
             <Brand
@@ -10,6 +17,39 @@ export default function Header({ children }: ChildrenProps) {
                 name='Blog'
             />
             <nav className={styles.navigation}>{children}</nav>
+            <nav
+                className={styles.navigation_dropdown}
+                onClick={toogleDropdown}>
+                <div className={styles.navigation_dropdown_icon}>
+                    {state ? (
+                        <Image
+                            src={'/icons/close.svg'}
+                            alt='Menu Icon'
+                            width={50}
+                            height={50}
+                        />
+                    ) : (
+                        <Image
+                            src={'/icons/menu.svg'}
+                            alt='Menu Icon'
+                            width={50}
+                            height={50}
+                        />
+                    )}
+                </div>
+                {state ? (
+                    <div className={styles.navigation_dropdown_children}>
+                        {children}
+                        <Button
+                            URL=''
+                            name='Login'
+                            type='filled'
+                        />
+                    </div>
+                ) : (
+                    <></>
+                )}
+            </nav>
             <div className={styles.call_to_action}>
                 <Button
                     URL=''
